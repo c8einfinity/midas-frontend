611fc4d64020d249a0aaa201e36cffb61d21a6f6
Kobus van Wyk <kobus.vanwyk.84@gmail.com>
2022-10-24 12:38:53 +0200

    DieselParts Big CSS Commit
    Final styling commit, pre-dev assistance.

--- app/design/frontend/CodeInfinity/Dieselpartsequip/registration.php
+++ app/design/frontend/CodeInfinity/Dieselpartsequip/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/CodeInfinity/DieselPartsEquip',
+    'frontend/CodeInfinity/Dieselpartsequip',
     __DIR__
 );
